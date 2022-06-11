09e79a8a7743078f4bb45f5a7d144ba8539fc299
Ally Rilling <ally.rilling@gmail.com>
2022-06-11 13:35:26 -0500

    updated homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,16 +2,21 @@ import React from 'react'
 import '../CSScomponents/Home.css'
 import { Col, Container, Row } from 'react-bootstrap'
 import me from "../images/me.png"
+import mail from "../images/mail.svg"
+import github from "../images/github.svg"
+import linkedin from "../images/linkedin.svg"
 
-const Mailto = ({ email, subject = '', body = '', children }) => {
+const Mailto = ( { email, subject = '', body = '', children } ) =>
+{
     let params = subject || body ? '?' : '';
-    if (subject) params += `subject=${encodeURIComponent(subject)}`;
-    if (body) params += `${subject ? '&' : ''}body=${encodeURIComponent(body)}`;
+    if ( subject ) params += `subject=${ encodeURIComponent( subject ) }`;
+    if ( body ) params += `${ subject ? '&' : '' }body=${ encodeURIComponent( body ) }`;
 
-    return <a href={`mailto:${email}${params}`}>{children}</a>;
+    return <a href={`mailto:${ email }${ params }`}>{children}</a>;
 };
 
-export default function Home() {
+export default function Home ()
+{
     return (
         <Container>
             <Row className="rowStyle">
@@ -20,9 +25,20 @@ export default function Home() {
                     <Row>
                         <Col>
                             <h1>Hello. My name is Ally.</h1>
-                            <p>I am a student at the University of Wisconsin - Madison majoring in Computer Science and Finance. Currently, I am working on an iOS app called Penguin Workouts. I am intersted in front end development, UI/UX design, and financial technologies.</p>
-                            <h2 className="linkText">Contact me on <a href="https://www.linkedin.com/in/allyrilling/">LinkedIn</a> or through <Mailto email="anrilling@wisc.edu" obfuscate={true}>email</Mailto> .</h2>
-                            <h2 className="linkText">See some of my work on <a href="https://github.com/allyrilling">GitHub</a>.</h2>
+                            <p>I am a student at the University of Wisconsin - Madison majoring in Computer Science and Finance. I am intersted in front end development, UI/UX design, and financial technologies.</p>
+                            {/* <h2 className="linkText">Contact me on <a href="https://www.linkedin.com/in/allyrilling/">LinkedIn</a> or through <Mailto email="anrilling@wisc.edu" obfuscate={true}>email</Mailto> .</h2>
+                            <h2 className="linkText">See some of my work on <a href="https://github.com/allyrilling">GitHub</a>.</h2> */}
+                            <Row>
+                                <a href='https://github.com/allyrilling' className='imgStyle'>
+                                    <img src={github}></img>
+                                </a>
+                                <a href='https://www.linkedin.com/in/allyrilling/' className='imgStyle'>
+                                    <img src={linkedin}></img>
+                                </a>
+                                <Mailto email='anrilling@wisc.edu' obfuscate={true}>
+                                    <img className='imgStyle' src={mail}></img>
+                                </Mailto>
+                            </Row>
                         </Col>
                     </Row>
                 </Col>
