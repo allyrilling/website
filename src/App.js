dfc98d448b169ee193363d84d5e50de4767a128e
Ally Rilling <ally.rilling@gmail.com>
2023-03-22 09:08:59 -0500

    revert to iframe

--- src/App.js
+++ src/App.js
@@ -560,7 +560,7 @@ function App() {
 													<h1>Origami Bird</h1>
 												</Col>
 												<Col>
-													<video width='800px' height='500px' src={origamiBird} />
+													<iframe width='800px' height='500px' src={origamiBird} />
 												</Col>
 											</Row>
 											<p></p>
@@ -569,7 +569,7 @@ function App() {
 													<h1>Snowboarding</h1>
 												</Col>
 												<Col>
-													<video width='800px' height='500px' src={snowboarding} />
+													<iframe width='800px' height='500px' src={snowboarding} />
 												</Col>
 											</Row>
 											<p></p>
@@ -578,7 +578,7 @@ function App() {
 													<h1>Track Cow</h1>
 												</Col>
 												<Col>
-													<video width='800px' height='500px' src={trackCow} />
+													<iframe width='800px' height='500px' src={trackCow} />
 												</Col>
 											</Row>
 										</Container>
